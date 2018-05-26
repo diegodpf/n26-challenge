5aba2fd7ae859c419a829aa7de496e2fedc6ecc6
fariasdiego <diegofarias@alunos.utfpr.edu.br>
2018-05-25 23:58:45 -0300

    More test cases were added

--- core/src/test/java/com/n26/challenge/transaction/TransactionServiceTest.java
+++ core/src/test/java/com/n26/challenge/transaction/TransactionServiceTest.java
@@ -11,8 +11,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static com.n26.challenge.utils.DateUtils.getCurrentTimestamp;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -44,16 +43,37 @@ public class TransactionServiceTest {
 
         verify(this.repository).addTransaction(transaction);
     }
+
+    @Test
+    public void addOldTransactionTest() {
+        long currentTimestamp = getCurrentTimestamp();
+        Transaction transaction = new Transaction(1, currentTimestamp - 61_000);
+
+        boolean success = this.service.addTransaction(transaction, currentTimestamp);
+
+        assertFalse(success);
+    }
+
     @Test
     public void getStatisticsSuccessfullyTest() {
         List<Transaction> transactions = new ArrayList<>();
         transactions.add(new Transaction(1, getCurrentTimestamp()));
+        transactions.add(new Transaction(2, getCurrentTimestamp()));
+        transactions.add(new Transaction(3, getCurrentTimestamp()));
+        transactions.add(new Transaction(4, getCurrentTimestamp()));
+        transactions.add(new Transaction(5, getCurrentTimestamp()));
+        transactions.add(new Transaction(6, getCurrentTimestamp() - 61_000));
 
         when(this.repository.getTransactions()).thenReturn(transactions);
 
         Statistics statistics = this.service.getStatistics(getCurrentTimestamp());
 
         assertNotNull(statistics);
+        assertEquals(15, statistics.getSum(), 0);
+        assertEquals(3, statistics.getAvg(), 0);
+        assertEquals(5, statistics.getMax(), 0);
+        assertEquals(1, statistics.getMin(), 0);
+        assertEquals(5, statistics.getCount());
 
         verify(this.repository).getTransactions();
     }
