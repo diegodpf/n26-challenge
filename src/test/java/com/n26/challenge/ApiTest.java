dcb3c0501223fb5363950c38feac3fe7edc46a11
fariasdiego <diegofarias@alunos.utfpr.edu.br>
2018-05-26 01:44:21 -0300

    Statistics integration test has been added

--- src/test/java/com/n26/challenge/ApiTest.java
+++ src/test/java/com/n26/challenge/ApiTest.java
@@ -1,5 +1,6 @@
 package com.n26.challenge;
 
+import com.n26.challenge.transaction.Transaction;
 import com.n26.challenge.transaction.TransactionRepository;
 import org.json.JSONObject;
 import org.junit.After;
@@ -12,13 +13,14 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import static com.n26.challenge.utils.DateUtils.getCurrentTimestamp;
 import static org.junit.Assert.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebAppConfiguration
@@ -57,7 +59,6 @@ public class ApiTest {
         this.mockMvc.perform(post("/transactions")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(request.toString()))
-                .andDo(print())
                 .andExpect(status().isCreated());
 
         assertEquals(1, this.repository.getTransactions().size());
@@ -74,9 +75,28 @@ public class ApiTest {
         this.mockMvc.perform(post("/transactions")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(request.toString()))
-                .andDo(print())
                 .andExpect(status().isNoContent());
 
         assertEquals(0, this.repository.getTransactions().size());
+    }
+
+    @Test
+    public void getStatisticsSuccessfullyTest() throws Exception {
+        this.repository.addTransaction(new Transaction(1, getCurrentTimestamp()));
+        this.repository.addTransaction(new Transaction(2, getCurrentTimestamp()));
+        this.repository.addTransaction(new Transaction(3, getCurrentTimestamp()));
+        this.repository.addTransaction(new Transaction(4, getCurrentTimestamp()));
+        this.repository.addTransaction(new Transaction(5, getCurrentTimestamp()));
+        this.repository.addTransaction(new Transaction(6, getCurrentTimestamp() - 61_000));
+
+        MvcResult result = this.mockMvc.perform(get("/statistics")).andExpect(status().isOk()).andReturn();
+
+        JSONObject response = new JSONObject(result.getResponse().getContentAsString());
+
+        assertEquals(15, response.getDouble("sum"), 0);
+        assertEquals(3, response.getDouble("avg"), 0);
+        assertEquals(5, response.getDouble("max"), 0);
+        assertEquals(1, response.getDouble("min"), 0);
+        assertEquals(5, response.getInt("count"));
     }
 }
